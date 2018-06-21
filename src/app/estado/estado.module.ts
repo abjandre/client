2dd967607ca140081d035be30146439a35b67ec3
aline <alinenichelle@hotmail.com>
2018-06-21 01:54:24 -0300

    estadoFucionario

--- src/app/estado/estado.module.ts
+++ src/app/estado/estado.module.ts
@@ -6,19 +6,25 @@ import {FormsModule} from '@angular/forms';
 import {DropdownModule} from 'primeng/primeng';
 import {EstadoService} from '../estado/estado.service';
 import {EstadoComponent} from './estado.component';
+import {LoginService} from '../login/login.service';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     TableModule,
-    DialogModule
+    DialogModule,
+    ConfirmDialogModule
   ],
   declarations: [
     EstadoComponent
   ],
   providers: [
-    EstadoService
+    EstadoService,
+    ConfirmationService,
+    LoginService
   ]
 })
 export class EstadoModule {
